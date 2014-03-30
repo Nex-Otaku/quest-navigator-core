0ba31721597f0e024d7a8492f6bb603826a8105b
Nex Otaku <nex@otaku.ru>
2014-03-30 11:02:18 +0400

    выкинул ненужный код

--- js/QspLibAwesomium.js
+++ js/QspLibAwesomium.js
@@ -10,54 +10,6 @@ var qspLibMode = "AWESOMIUM";       // "AIR", "PHONEGAP", "AWESOMIUM" - уста
 
 var QspLib = QspLibAwesomium;
 
-var oldLib = {
-    restartGame: function() {
-        return cordova.exec(null, null, "QspLibAwesomium", "restartGame", []);
-    },
-
-    executeAction: function(index) {
-        return cordova.exec(null, null, "QspLibAwesomium", "executeAction", [index]);
-    },
-    
-    selectObject: function(index) {
-        return cordova.exec(null, null, "QspLibAwesomium", "selectObject", [index]);
-    },
-    
-    loadGame: function() {
-        return cordova.exec(null, null, "QspLibAwesomium", "loadGame", []);
-    },
-    
-    saveGame: function() {
-        return cordova.exec(null, null, "QspLibAwesomium", "saveGame", []);
-    },
-    
-    saveSlotSelected: function(index, open) {
-        var mode = open ? 1 : 0;
-        return cordova.exec(null, null, "QspLibAwesomium", "saveSlotSelected", [index, mode]);
-    },
-
-    msgResult: function() {
-        return cordova.exec(null, null, "QspLibAwesomium", "msgResult", []);
-    },
-    
-    errorResult: function() {
-        return cordova.exec(null, null, "QspLibAwesomium", "errorResult", []);
-    },
-    
-    userMenuResult: function(index) {
-        return cordova.exec(null, null, "QspLibAwesomium", "userMenuResult", [index]);
-    },
-    
-    inputResult: function(text) {
-        return cordova.exec(null, null, "QspLibAwesomium", "inputResult", [text]);
-    },
-
-    setMute: function(mute) {
-        return cordova.exec(null, null, "QspLibAwesomium", "setMute", [mute]);
-    }
-};
-
-
 function onWebDeviceReady() {
 	// Самодельный диалог alert, 
 	// так как в Awesomium стандартные диалоги не работают.
