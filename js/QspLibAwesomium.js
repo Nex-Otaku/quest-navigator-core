6ce70784d08a01e99a9f29d064f3939dd36aa2a2
Nex Otaku <nex@otaku.ru>
2014-08-11 21:20:17 +0400

    исправлен баг с запуском плеера

--- js/QspLibAwesomium.js
+++ js/QspLibAwesomium.js
@@ -8,9 +8,24 @@ var qspLibMode = "AWESOMIUM";       // "AIR", "PHONEGAP", "AWESOMIUM" - уста
                                    // чтобы api.js мог выполнять различный код в зависимости от платформы
 
 
-var QspLib = QspLibAwesomium;
+var QspLib = null;
 
 function onWebDeviceReady() {
+	if (QspLib !== null) {
+		throw "onWebDeviceReady must be called only once!";
+	}
+	// Эта функция вызывается из движка Awesomium
+	// в обработчике завершения загрузки страницы.
+	// Только к этому моменту мы можем быть уверены,
+	// что созданный движком Awesomium библиотечный объект "QspLibAwesomium"
+	// уже загружен и нормально обработает вызовы колбэков.
+	// Событие "$(document).ready" срабатывает раньше.
+	if (typeof QspLibAwesomium == 'undefined') {
+		throw "QspLibAwesomium is not available in \"document.ready\"";
+	}
+	QspLib = QspLibAwesomium;
+	// Запускаем API.
+	qspInitApi();
 	// Самодельный диалог alert, 
 	// так как в Awesomium стандартные диалоги не работают.
 	// Короткий вариант будет работать только после полной инициализации.
@@ -28,11 +43,7 @@ function debug(str) {
 
 function qspLibOnInitApi() {
 	setTimeout( function() { // Delay for Mozilla
-		onWebDeviceReady();
+		// Запуск игры по завершению инициализации API.
+		QspLib.restartGame();
 	}, 10);
 }
-
-/* Запуск API при загрузке страницы */
-$(document).ready(function(){
-		qspInitApi();
-    });
